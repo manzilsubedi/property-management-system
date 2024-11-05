a6ea5216645dfc0f03c8c7131ff8b126266c10e9
Mahesh Subedi <mnzlsubedi@gmail.com>
2024-11-05 14:33:26 +0000

    property crud feature

---
+++ property-management-api/Data/Repositories/PropertyRepository.cs
@@ -0,0 +1,27 @@
+﻿using property_management_api.Models;
+
+namespace property_management_api.Data.Repositories
+{
+    public class PropertyRepository : IPropertyRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PropertyRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddPropertyAsync(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> GetAllPropertiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Property> GetPropertyByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
